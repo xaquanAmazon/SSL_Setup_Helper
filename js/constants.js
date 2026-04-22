1b0e4e39a2360e9e0a1fea59a5a16e46f6690760
Xa Quan <xaquan@amazon.com>
2026-04-22 14:14:16 -0700

    added back assets/stations

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,8 @@
 export const QR_SIZE = 128;
 export const CSS_SRC = 'styles.css';
 export const QR_CODE_SRC = 'js/qrcode.min.js';
-export const DEFAULT_WORK_CENTER_URL = 'https://MESURL/index.html?SITE=S{SITE}&WORKCENTER={Work_Center_ID}';
+export const STATION_SRC = 'assets/stations.csv';
+export const DEFAULT_WORK_CENTER_URL = 'https://manufacture.prod.mes.kbobjects.com/XMII/CM/KUIPER/App/POD/webapp/index.html?SITE=S{SITE}&WORKCENTER={Work_Center_ID}';
 export const MES_SFC_SEARCH_INPUT_ID = 'container-kuiper.pod_lite---Main--sfcSelect-I';
 export const STORAGE_KEYS = ['savedData', 'LayerNumber', 'Rows', 'Cols', 'WorkCenterId', 'SiteId'];
 export const SETTINGS_KEYS = ['stationsCsv', 'workSites', 'workCenterUrlTemplate'];
